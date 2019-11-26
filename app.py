d9881f034469e6370f5d6961db873e37284a69ec
ZhongminYao <40215303+ZhongminYao@users.noreply.github.com>
2019-11-26 03:24:35 -0500

    connect MongoDB

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,13 +6,13 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+import mongodb
+import re
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('W4GASPbfZc5cAZqcezDY8/MeqMqwOf9fGBDN+ARBg2r+Wprku8D6qRVNT435jH1A5fqv6Mq931FAMljSXWi+64yP0V8XTa3dldCvGG/cwa79LUsvCpi/bJYCNnpbIzToTYaD4jbdM2s5GxiG+ctPMwdB04t89/1O/w1cDnyilFU=')
-
 handler = WebhookHandler('52c7933d915bbea23767c2c6f5982c45')
-
 line_bot_api.push_message('Uc3b6aa3866a6acf316e848328f5404ca', TextSendMessage(text='Good to go!'))
 
 
@@ -33,8 +32,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id 
+    usespeak=str(event.message.text) 
+    if re.match('[0-9]{4}[<>][0-9]',usespeak): 
+        mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
+        line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+'successfully saved!'))
+        return 0
+
+    
+    elif re.match('delete[0-9]{4}',usespeak): 
+        mongodb.delete_user_stock_fountion(stock=usespeak[2:])
+        line_bot_api.push_message(uid, TextSendMessage(usespeak+'successfully deleted!'))
+        return 0
+
 
 import os
 if __name__ == "__main__":
---
+++ mongodb.py
@@ -0,0 +1,40 @@
+
+from pymongo import MongoClient
+import urllib.parse
+import datetime
+
+Authdb='資料庫名稱'
+
+
+def constructor():
+    client = MongoClient("mongodb://dickyao98:<Win19980714!>@financechatbot-shard-00-00-q2kn9.mongodb.net:27017,financechatbot-shard-00-01-q2kn9.mongodb.net:27017,financechatbot-shard-00-02-q2kn9.mongodb.net:27017/test?ssl=true&replicaSet=FinanceChatBot-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client[Authdb]
+    return db
+   
+
+def write_user_stock_fountion(stock, bs, price):  
+    db=constructor()
+    collect = db['mystock']
+    collect.insert({"stock": stock,
+                    "data": 'care_stock',
+                    "bs": bs,
+                    "price": float(price),
+                    "date_info": datetime.datetime.utcnow()
+                    })
+    
+
+def delete_user_stock_fountion(stock):  
+    db=constructor()
+    collect = db['mystock']
+    collect.remove({"stock": stock})
+    
+
+def show_user_stock_fountion():  
+    db=constructor()
+    collect = db['mystock']
+    cel=list(collect.find({"data": 'care_stock'}))
+
+    return cel
+
+
+
